117b5f9ecb14684d79641ece75c65ce40cf34c35
marvenp <mparmar@lanterncapital.ca>
2023-05-23 16:34:26 -0400

    Testting 5

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 
 import './App.css';
+import logo from './lanternLogo.svg';
 
 function App() {
   return (
     <div className="App">
-      
-        Testing 
+       <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Join <code>Lantern</code> and get your client exciting Deals.
+        </p>
+        <a
+          className="App-link"
+          href="/#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Know Lantern
+        </a>
+      </header>
+    </div>
     </div>
     
   );
